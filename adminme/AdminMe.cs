15027f0e370c116bd991b583eae9de0b0b821ea2
ElementalVenom <elementalvenom1@gmail.com>
2015-12-29 16:00:25 -0600

    first release

---
+++ adminme/AdminMe.cs
@@ -0,0 +1,40 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Core.Logging;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Chat;
+using UnityEngine;
+using Rocket.API;
+using System;
+
+namespace AdminMe
+{
+    public class AdminMe : RocketPlugin
+    {
+        public static AdminMe Instance;
+        protected override void Load()
+        {
+            U.Events.OnPlayerConnected += PlayerConnected;
+            U.Events.OnPlayerDisconnected += PlayerDisconnected;
+            Instance = this;
+        }
+        void PlayerConnected(UnturnedPlayer player)
+        {
+            if (player.IsAdmin)
+            {
+                player.GodMode = false;
+                player.VanishMode = false;
+                player.Admin(false);
+            }
+        }
+        void PlayerDisconnected(UnturnedPlayer player)
+        {
+            if (player.IsAdmin)
+            {
+                player.GodMode = false;
+                player.VanishMode = false;
+                player.Admin(false);
+            }
+        }
+    }
+}
---
+++ adminme/CommandAdminme.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core.Plugins;
+using Rocket.Core;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket;
+using Rocket.Unturned.Chat;
+
+namespace AdminMe
+{
+    class CommandAdminme : IRocketCommand
+    {
+        public string Name
+        {
+            get { return "adminme"; }
+        }
+        public string Help
+        {
+            get { return "Gives a player admin"; }
+        }
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Player; }
+        }
+        public string Syntax
+        {
+            get { return ""; }
+        }
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>()
+                {
+                    "adminme",
+                };
+            }
+        }
+        public void Execute(Rocket.API.IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if (player.HasPermission("adminme"))
+            {
+                if (player.IsAdmin)
+                {
+                    player.Admin(false);
+                    player.GodMode = false;
+                    player.VanishMode = false;
+                    UnturnedChat.Say(player.CharacterName + " is now off duty.", UnityEngine.Color.red);
+                }
+                else
+                {
+                    player.Admin(true);
+                    player.GodMode = true;
+                    UnturnedChat.Say(player.CharacterName + " is now on duty.", UnityEngine.Color.red);
+                }
+            }
+        }
+    }
+}
